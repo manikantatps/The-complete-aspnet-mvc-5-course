2281f655e8bda276590fa70cc21589dd84a9698c
manikantatps <manikantatps@gmail.com>
2018-08-27 23:25:23 +0530

    Session 2 - "17 Partial View"

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -13,6 +13,22 @@ namespace Vidly
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            //Custom route for movies controller
+            //routes.MapRoute(
+            //    "MoviesByReleaseDate",
+            //    "movies/released/{year}/{month}",
+            //    new { Controller = "Movies", Action = "ByReleaseDate" },
+            //    new { year = @"\d{4}", month = @"\d{2}" });
+            //this will accept only year as 2015 or 2016
+            //routes.MapRoute(
+            //    "MoviesByReleaseDate",
+            //    "movies/released/{year}/{month}",
+            //    new { Controller = "Movies", Action = "ByReleaseDate" },
+            //    new { year = @"2015|2016", month = @"\d{2}" });
+
+            //If we write "MapMvcAttributeRoutes" we can write route in controller itself instead of in RouteConfig.cs file
+            routes.MapMvcAttributeRoutes();
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,16 +4,77 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
+        // GET: Movies/Random
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
-            return View(movie);
+            //return View(movie);
+            //Content Will just return the text
+            //return Content("Hello World!!!!");
+            //HttpNotFound will return 404 not found page
+            //return HttpNotFound();
+            //EmptyResult return nothing
+            //return new EmptyResult();
+            //RedirectToAction navigates the controller action as specified Parameters are action, controller and parameters(parameters will be shown in URL)
+            //return RedirectToAction("Contact", "Home", new { page = 1, sortBy = "name" });
+
+            //View Data
+            //View data is a old approach and "Movie" is changed it will throughts a null error. Instead of this passing model is the best way.
+            //ViewData["Movie"] = movie;
+            //return View();
+
+            //ViewModel Example
+
+            var customers = new List<Customer>
+            {
+                new Customer{Name="Customer 1" },
+                new Customer{Name="Customer 2" },
+                new Customer{Name="Customer 3" }
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
+        // GET: Movies/Edit/{id}
+        public ActionResult Edit(int id)
+        {
+            return Content("Id=" + id);
+        }
+
+        //This is for optional parameters
+        // GET: Movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "name";
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+        //Example for custom route and the cusrom route is added in "RouteConfig.cs"
+        // GET: Movies/Released
+        //Below Route is without constraints
+        //[Route("movies/released/{year}/{month}")]
+        //Below Route is with constraints below regullar expression will accepts only 2 digit number for month
+        //[Route("movies/released/{year}/{month:regex(\\d{2})}")]
+        //Below Route is with multiple constraints below regullar expression will accepts only 2 digit number for month and range will be 1 to 12
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
         }
     }
 }
